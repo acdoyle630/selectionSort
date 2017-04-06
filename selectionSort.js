2148a2da122eacb7fce0502a74bd8a1884a75007
Adam <acdoyle630@gmail.com>
2017-04-06 13:37:32 -1000

    selection sort tests fixed, return added to recusrion and CL cleaned

--- selectionSort.js
+++ selectionSort.js
@@ -3,12 +3,17 @@
 module.exports = () => {
   var min = 0;
   function selectionSort( arr ){
+    if(Array.isArray(arr) !== true){
+      return false;
+    }
   if( min === arr.length - 1 ){
-    console.log( arr);
     return arr;
   }
   var smallest = arr[min];
   for (var i = min; i < arr.length; i ++){
+    if(typeof arr[i] !== 'number'){
+      return false;
+    }
     var current;
     var prev;
       if ( arr[i] < smallest ) {
@@ -19,12 +24,10 @@ module.exports = () => {
     }
   }
   min ++;
-  selectionSort(arr);
+  return selectionSort(arr);
   }
   return{
     selectionSort,
   };
 };
 
-var test = module.exports();
-test.selectionSort([3,2,5,7,99,-9]);
